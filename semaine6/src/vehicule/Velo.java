a2124cb4e0b17a7bffd39b770962336b5220cf1a
Junji <qq870185814@163.com>
2020-11-03 12:40:44 +0100

    tme 6 update

--- semaine6/src/vehicule/Velo.java
+++ semaine6/src/vehicule/Velo.java
@@ -1,20 +1,18 @@
 public class Velo extends Vehicule{
     protected double vitesse;
-    protected int nbPlaces;
 
     public Velo(double v, int np) {
         super();
         vitesse = v;
-        nbPlaces = np;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " :: Velo [nbPlaces=" + nbPlaces + ", vitesse=" + vitesse + "]";
+        return super.toString() + " :: Velo [vitesse=" + vitesse + "]";
     }
 
     public void transporter(String depart, String arrivee){
         System.out.println("Le vélo n°" + super.getNumero() + 
-        " a transporté " + nbPlaces + " personne de " + depart + " à " + arrivee + ".");
+        " a roulé de " + depart + " à " + arrivee + ".");
     }
 }
