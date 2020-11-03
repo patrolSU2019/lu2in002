783bccd78d9228b449ed65753a602439f6df83a3
Junji <qq870185814@163.com>
2020-11-03 14:44:15 +0100

    nothing

--- semaine6/src/orchestre/Piano.java
+++
@@ -1,9 +0,0 @@
-public class Piano extends Instrument{
-    
-    public Piano(double poids, double prix) {
-        super(poids, prix);
-    }
-    public void jouer(){
-        System.out.println("Le piano joue.");
-    }  
-}
