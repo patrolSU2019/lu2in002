5a333f9ef676dbe1f47895bc428794aa0f7fff76
Junji <qq870185814@163.com>
2020-11-03 10:55:57 +0100

    java td 6 update

--- semaine6/src/personne/TestPersonne.java
+++ semaine6/src/personne/TestPersonne.java
@@ -1,9 +1,10 @@
 public class TestPersonne {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        Personne p = new Salarie("Albert");
+        Personne p = new Personne("Albert");
         p.ajouterEnfant();
-        System.out.println(p.prime());
-        System.out.println(p.estEnL2());
+        System.out.println(p);
+        // System.out.println(p.prime());
+        // System.out.println(p.estEnL2());
     }
 }
