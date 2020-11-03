7f314387beeab5b2f2c24be45257150b154f5fbf
Junji <qq870185814@163.com>
2020-11-03 14:41:24 +0100

    java tme 6 update

--- semaine6/src/orchestre/Trompette.java
+++
@@ -1,9 +0,0 @@
-public class Trompette extends Instrument {
-    public Trompette(double poids, double prix) {
-        super(poids, prix);
-    }
-    
-    public void jouer(){
-        System.out.println("La trompette joue.");
-    }
-}
